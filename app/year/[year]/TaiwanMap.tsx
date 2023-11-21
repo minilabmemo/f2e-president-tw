c7f51a8928df09e5c129cb0c1275a8689fdaea65
minilabmemo <minilabmemo@gmail.com>
2023-11-21 16:19:30 +0800

    Fix: tidy TaiwanMap

--- app/year/[year]/TaiwanMap.tsx
+++ app/year/[year]/TaiwanMap.tsx
@@ -1,101 +1,6 @@
 
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
-
-async function getData() {
-  try {
-    const result = await fetch('/taiwan.geojson');
-    if (result.ok) {
-      console.log("🚀 ~ file: TaiwanMap.tsx:8 ~ getData ~ result:", result)
-      console.log("🚀 ~ file: TaiwanMap.tsx:8 ~ getData ~ ok:")
-      return result.json();
-    }
-  } catch (error) {
-    console.log(`error: ${error}`);
-  }
-
-  return [];
-}
-
-
-export default async function TaiwanMap() {
-
-  // const name = await getData()
-  // console.log("🚀 ~ file: TaiwanMap.tsx:32 ~ name:", name)
-
-  const mapRef = useRef<HTMLDivElement | null>(null);
-  const hasFetchedData = useRef(false); // 新增一個 ref 來追蹤是否已經獲取過數據
-
-  useEffect(() => {
-
-    const width = 900;
-    const height = 600;
-
-    const svg = d3
-      .select(mapRef.current)
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('id', 'svg');
-
-    const projection = d3.geoMercator()
-      .scale(6000)
-      .center([121, 24])
-      .translate([width / 3, height / 2]);
-
-    const pathGenerator = d3.geoPath().projection(projection);
-    if (!hasFetchedData.current) { // 只有在尚未獲取數據的情況下執行
-      fetch("/taiwan.geojson") // 讀取在/public 下的資源
-        .then(response => response.json())
-        .then(data => {
-          hasFetchedData.current = true; // 標記已經獲取過數據
-          const taiwanGeoJSON: GeoJSONFeature[] = data.features;
-          console.log("🚀 ~ file: TaiwanMap.tsx:51 ~ useEffect ~ taiwanGeoJSON:", taiwanGeoJSON)
-
-          svg
-            .selectAll('path')
-            .data(taiwanGeoJSON)
-            .enter()
-            .append('path')
-            .attr('d', (d: any) => pathGenerator(d.geometry)!)
-            .attr('id', (d) => 'city' + d.properties.COUNTYCODE)
-            .on('click', (d) => {
-              console.log(d.properties.COUNTYNAME);
-              document.querySelector('h2')!.textContent = d.properties.COUNTYNAME;
-              document.querySelector('h3')!.textContent = d.properties.COUNTYENG;
-
-              const activeElement = document.querySelector('.active');
-              if (activeElement) {
-                activeElement.classList.remove('active');
-              }
-
-              const cityElement = document.getElementById('city' + d.properties.COUNTYCODE);
-              if (cityElement) {
-                cityElement.classList.add('active');
-              }
-            });
-        })
-        .catch(error => {
-          console.error('Error loading GeoJSON data:', error);
-        });
-    }
-  }, []);
-
-  return (
-    <div className="container-map">
-      <div className="taiwan-map">
-        <div ref={mapRef} id="map"></div>
-      </div>
-      <div className="shop-list">
-        <h2></h2>
-        <h3></h3>
-      </div>
-    </div>
-  );
-};
-
-//export default TaiwanMap;
-
 interface GeoJSONProperties {
   COUNTYCODE: string;
   COUNTYNAME: string;
@@ -111,3 +16,81 @@ interface GeoJSONFeature {
   properties: GeoJSONProperties;
   geometry: GeoJSONGeometry;
 }
+export default function TaiwanMap() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const hasFetchedData = useRef(false); // 新增一個 ref 來追蹤是否已經獲取過數據
+
+
+  useEffect(() => {
+
+    const width = 900;
+    const height = 800;
+
+    if (!hasFetchedData.current) { // 只有在尚未獲取數據的情況下執行
+      console.log("🚀 ~ file: TaiwanMap.tsx:18 ~ useEffect ~ useEffect:", hasFetchedData.current)
+
+      const svg = d3
+        .select(mapRef.current)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('id', 'svg');
+
+      const projection = d3.geoMercator()
+        .scale(6000)
+        .center([121, 24])
+        .translate([width / 3, height / 2]);
+
+      const pathGenerator = d3.geoPath().projection(projection);
+      hasFetchedData.current = true;
+      fetch("/taiwan.geojson") // 讀取在/public 下的資源
+        .then(response => response.json())
+        .then(data => {
+          const taiwanGeoJSON: GeoJSONFeature[] = data.features;
+          svg
+            .selectAll('path')
+            .data(taiwanGeoJSON)
+            .enter()
+            .append('path')
+            .attr('d', (d: any) => pathGenerator(d.geometry)!)
+            .attr('id', (d: any) => 'city' + d.properties.COUNTYCODE)
+            .on('click', (event, data) => {
+              console.log(data);
+              document.querySelector('h2')!.textContent = data.properties.COUNTYNAME;
+              document.querySelector('h3')!.textContent = data.properties.COUNTYENG;
+
+              const activeElement = document.querySelector('.active');
+              if (activeElement) {
+                activeElement.classList.remove('active');
+              }
+
+              const cityElement = document.getElementById('city' + data.properties.COUNTYCODE);
+              if (cityElement) {
+                cityElement.classList.add('active');
+              }
+            });
+        })
+        .catch(error => {
+          console.error('Error loading GeoJSON data:', error);
+        });
+      return () => {
+        svg.selectAll('path').remove();
+
+      };
+    }
+  });
+
+  return (
+    <div className="container-map">
+      <div className="taiwan-map">
+        <div ref={mapRef} id="map"></div>
+      </div>
+      <div className="shop-list">
+        <h2></h2>
+        <h3></h3>
+      </div>
+    </div>
+  );
+};
+
+
