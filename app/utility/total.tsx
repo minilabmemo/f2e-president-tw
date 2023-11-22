e7e4f0994b58b2135319fb676673999fa1c371eb
minilabmemo <minilabmemo@gmail.com>
2023-11-22 19:05:11 +0800

    Fix: images to public

--- app/utility/total.tsx
+++ app/utility/total.tsx
@@ -1,7 +1,4 @@
 
-
-import role from "../../assets/images/role.png"
-
 import Image from 'next/image'
 export type TotalItem = {
   party: string;
@@ -26,21 +23,21 @@ export const result_2020: TotalItem[] = [
     party: '蝙蝠黨',
     name: '德古拉',
     number: '8,170,231',
-    imageNode: <Image src={role} alt="role" />,
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />,
     color: "blue-150"
   },
   {
     party: '弓箭黨',
     name: '林克',
     number: '3,522,119',
-    imageNode: <Image src={role} alt="role" />,
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />,
     color: "orange-150"
   },
   {
     party: '木棍黨',
     name: '綠巨人',
     number: '5,522,119',
-    imageNode: <Image src={role} alt="role" />,
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />,
     color: "green-150"
   }
 
@@ -51,7 +48,7 @@ export const result_2016: TotalItem[] = [
     party: '蝙蝠黨',
     name: '德 16',
     number: '8,170,231',
-    imageNode: <Image src={role} alt="role" />
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />
 
 
   },
@@ -59,19 +56,19 @@ export const result_2016: TotalItem[] = [
     party: '弓箭黨',
     name: '林克',
     number: '3,522,119',
-    imageNode: <Image src={role} alt="role" />
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />
   },
   {
     party: '木棍黨',
     name: '綠巨人',
     number: '5,522,119',
-    imageNode: <Image src={role} alt="role" />
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />
   },
   {
     party: '木棍黨',
     name: '綠巨人',
     number: '5,522,119',
-    imageNode: <Image src={role} alt="role" />
+    imageNode: <Image src="/images/role.png" width={48} height={48} alt="role" />
   },
 
 ];
