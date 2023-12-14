a0353f57925a353d6ba3973f93dcd1d032a8696d
minilabmemo <minilabmemo@gmail.com>
2023-12-14 13:04:59 +0800

    Add: loading

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <>
+      <h1 className="text-pink-200 text-3xl">Loading...</h1>
+    </>
+  );
+}
---
+++ app/year/[year]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <>
+      <h1 className="text-blue-150 text-3xl">Loading...</h1>
+    </>
+  );
+}
